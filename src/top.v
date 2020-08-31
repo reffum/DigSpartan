642e3a6ed37d4fac6da0d959081c9124511944d1
gavrilchenko <Gavrilchenko@avionica.spb.ru>
2020-08-31 17:31:33 +0300

    Add all needed pins.

--- src/top.v
+++ src/top.v
@@ -7,6 +7,7 @@ module top(
     output uart_tx,
     input uart_rx,
     output [7:0] led,
+    input [1:0] button,
     
     input [3:0] lvds_in_p,
     input [3:0] lvds_in_n,
@@ -18,6 +19,51 @@ module top(
     input lvds_clk_n
     );
     
+    wire lvds_clk;
+    wire [3:0] lvds_in;
+    
     assign led = 8'hFF;
-        
+    
+    IBUFGDS 
+    #(
+       .DIFF_TERM("TRUE"),
+       .IOSTANDARD("LVDS_33")  
+    ) 
+    IBUFGDS_LVDS_CLK
+    (
+       .O(lvds_clk),
+       .I(lvds_clk_p),
+       .IB(lvds_clk_p)
+    );
+    
+    // LVDS input and output
+    genvar i;
+    generate
+        for(i = 0; i < 4; i = i + 1) begin        
+            IBUFDS 
+            #(
+               .DIFF_TERM("TRUE"),
+               .IOSTANDARD("LVDS_33")
+            ) 
+            IBUFDS_LVDS_IN 
+            (
+               .O(O),
+               .I(lvds_in_p[i]),
+               .IB(lvds_in_n[i])
+            );
+            
+            OBUFDS #(
+               .IOSTANDARD("LVDS_33")
+            ) OBUFDS_inst (
+               .O(lvds_out_p[i]),
+               .OB(lvds_out_n[i]),
+               .I(lvds_in[i]) 
+            );            
+                             
+        end
+    endgenerate                   
 endmodule
+
+
+    					
+    					
